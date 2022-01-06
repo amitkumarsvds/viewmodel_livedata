848a6580cdf08b901716dcef727f66427bae1c0a
unknown <=>
2022-01-06 16:11:47 +0530

    adding more comments

--- MYNotification/app/src/main/java/com/example/amitkumar/mynotification/MainActivity.java
+++ MYNotification/app/src/main/java/com/example/amitkumar/mynotification/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//sanjeeb made changes
+        setContentView(R.layout.activity_main);//sanjeeb made changes. adding more comments
     }
 }
